d5144408e3fe7d9ca24a8202d5f7f4f18a754f7a
michael <aoinihonjin@gmail.com>
2025-04-22 10:46:44 +0300

    did the bare minimum lol

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const todoForm = document.getElementById("create-task-form");
+  const todoList = document.getElementById("tasks");
+  
+  todoForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const taskInput = document.getElementById("new-task-description");
+    const newTaskText = taskInput.value;
+    
+    if (newTaskText.trim() !== "") {
+      const newTask = document.createElement("li");
+      newTask.textContent = newTaskText;
+      
+      todoList.appendChild(newTask);
+      
+      taskInput.value = "";
+    }
+  });
 });
